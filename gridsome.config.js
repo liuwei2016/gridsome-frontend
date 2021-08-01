5114835206465dda47c2c9a46fa8bfe2923c949e
liuwei2015 <liuwei2015@daojia-inc.com>
2021-08-01 23:07:44 +0800

    feat: 修改配置

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'atdawei',
   plugins:[
     {
         use:'@gridsome/source-filesystem',
